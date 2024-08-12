be81f0b470e4347d58a1d9b220b9765e3025693f
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2024-08-12 15:16:59 +0600

    Moved Crops to controll

--- Controller/Runtime/Items/Crop.cs
+++ Controller/Runtime/Items/Crop.cs
@@ -1,9 +1,10 @@
 ﻿using QuickEye.Utility;
+using Soul.Model.Runtime.Items;
 using UnityEngine;
 
-namespace Soul.Model.Runtime.Items
+namespace Soul.Controller.Runtime.Items
 {
-    [CreateAssetMenu(fileName = ".crop", menuName = "Soul/Item/Create Crop")]
+    [CreateAssetMenu(fileName = "crop", menuName = "Soul/Item/Create Crop")]
     public class Crop : Item, IWeight
     {
         public UnityTimeSpan growTime;
